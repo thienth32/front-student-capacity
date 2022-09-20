b51addaf0f8856926da36a2e22161e8f27c6172b
vanquang202 <83849198+vanquang202@users.noreply.github.com>
2022-09-20 12:08:50 +0700

    up socket

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,32 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Echo from 'laravel-echo';
+declare global {
+  interface Window {
+    Echo: any;
+  }
+}
+
+window.Echo = new Echo({
+  broadcaster: 'socket.io',
+  host: `${window.location.protocol}//${window.location.hostname}:6001`,
+  withCredentials: true,
+  // auth: {
+  //   headers: {
+  //     Authorization: "Bearer " + token,
+  //   },
+  // },
+});
+
+window.Echo.channel('public.channel').listen('PublicChannel', (data: any) => {
+  console.log(data);
+});
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
